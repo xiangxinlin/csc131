eab0c5e74983154d567e71d23aef288570a981ef
nickduong <160074016+nickduong@users.noreply.github.com>
2024-03-01 13:44:37 -0800

    Update spoonacularApiTesting.java

--- spoonacularApiTesting.java
+++ spoonacularApiTesting.java
@@ -6,10 +6,8 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
 
-
 public class spoonacularApiTesting {
-    
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter your query: ");
         String findQuery = scan.nextLine();
@@ -25,16 +23,16 @@ public class spoonacularApiTesting {
             int responseCode = conn.getResponseCode();
             if (responseCode != 200) {
                 throw new RuntimeException("HttpResponseCode: " + responseCode);
-            }else{
+            } else {
                 StringBuilder infoString = new StringBuilder();
                 BufferedReader read = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
                 String line;
-                while ((line = read.readLine()) != null){
+                while ((line = read.readLine()) != null) {
                     infoString.append(line);
                 }
                 read.close();
-                // parse the JSON manually given from API
+
                 String jsonResponse = infoString.toString();
                 String[] recipes = jsonResponse.split("\"results\":\\[")[1].split("\\],\"offset\"")[0].split("\\},\\{");
 
@@ -44,14 +42,38 @@ public class spoonacularApiTesting {
                     String title = recipe.split("\"title\":\"")[1].split("\",\"")[0];
                     String image = recipe.split("\"image\":\"")[1].split("\",\"")[0];
                     System.out.println(recipeNumber + ". " + title + " - " + image);
-                    recipeNumber ++;
+                    recipeNumber++;
+                }
+
+                System.out.println("Enter the numbers of the recipes you want to save, separated by spaces, or enter 'none' to skip:");
+                String input = scan.nextLine();
+
+                if (!input.equalsIgnoreCase("none")) {
+                    String[] selectedRecipeNumbers = input.split("\\s+");
+                    for (String recipeNumberStr : selectedRecipeNumbers) {
+                        int selectedRecipeNumber = Integer.parseInt(recipeNumberStr);
+                        if (selectedRecipeNumber > 0 && selectedRecipeNumber <= recipes.length) {
+                            String selectedRecipe = recipes[selectedRecipeNumber - 1];
+                            String title = selectedRecipe.split("\"title\":\"")[1].split("\",\"")[0];
+                            String image = selectedRecipe.split("\"image\":\"")[1].split("\",\"")[0];
+                            String id = selectedRecipe.split("\"id\":")[1].split(",")[0];
+                            String imageType = selectedRecipe.split("\"imageType\":\"")[1].split("\",\"")[0];
+
+                            recipeSaver recipeSaver = new recipeSaver();
+                            recipeSaver.saveRecipe(Integer.parseInt(id), title, image, imageType, true);
+                            System.out.println("Recipe '" + title + "' saved successfully!");
+                        } else {
+                            System.out.println("Invalid recipe number: " + selectedRecipeNumber + ". Skipping...");
+                        }
+                    }
+                } else {
+                    System.out.println("No recipe saved.");
                 }
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally{
+        } finally {
             scan.close();
         }
     }
 }
-
