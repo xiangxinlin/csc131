b7ddb10413a13a500ead9b3f6e67fb101bbe6c9c
nickduong <160074016+nickduong@users.noreply.github.com>
2024-03-27 14:39:11 -0700

    Update searchByTitle.java refactored it so save and view are now a new class called recipeInteraction

--- searchByTitle.java
+++ searchByTitle.java
@@ -11,9 +11,9 @@ import org.bson.Document;
 //class currently responsible for interacting with the API to search for recipes
 public class searchByTitle {
     public void searchTitle() {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your query: ");
-        String findQuery = scan.nextLine();
+        String findQuery = scanner.nextLine();
 
         try {
             //API key for accessing 
@@ -54,57 +54,12 @@ public class searchByTitle {
                     recipeNumber++;
                 }
 
-                //ask if user wants to save recipes
-                System.out.println("Enter the numbers of the recipes you want to save, separated by spaces, or enter 'none' to skip:");
-                String input = scan.nextLine();
-
-                //process if the user input for saving recipes
-                if (!input.equalsIgnoreCase("none")) {
-                    String[] selectedRecipeNumbers = input.split("\\s+");
-                    for (String recipeNumberStr : selectedRecipeNumbers) {
-                        int selectedRecipeNumber = Integer.parseInt(recipeNumberStr);
-                        if (selectedRecipeNumber > 0 && selectedRecipeNumber <= recipes.length) {
-                            String selectedRecipe = recipes[selectedRecipeNumber - 1];
-                            String title = selectedRecipe.split("\"title\":\"")[1].split("\",\"")[0];
-                            String image = selectedRecipe.split("\"image\":\"")[1].split("\",\"")[0];
-                            String id = selectedRecipe.split("\"id\":")[1].split(",")[0];
-                            String imageType = selectedRecipe.split("\"imageType\":\"")[1].split("\",\"")[0];
-
-                            recipeSaver recipeSaver = new recipeSaver();
-                            //save the selected recipe 
-                            recipeSaver.saveRecipe(Integer.parseInt(id), title, image, imageType, true);
-                            System.out.println("Recipe '" + title + "' saved successfully!");
-                        } else {
-                            System.out.println("Invalid recipe number: " + selectedRecipeNumber + ". Skipping...");
-                        }
-                    }
-                } else {
-                    System.out.println("No recipe saved.");
-                }
-                
-                // Ask the user if they want to view their list of saved recipes
-                System.out.println("Do you want to view your list of saved recipes? (yes/no)");
-                String viewListInput = scan.nextLine();
-                if (viewListInput.equalsIgnoreCase("yes")) {
-                    //displays saved recipe if user typed yes
-                    recipeSaver recipeSaver = new recipeSaver();
-                    List<Document> savedRecipes = recipeSaver.getSavedRecipes();
-                    if (!savedRecipes.isEmpty()) {
-                        System.out.println("Your list of saved recipes:");
-                        int index = 1;
-                        for (Document savedRecipe : savedRecipes) {
-                            String savedTitle = savedRecipe.getString("title");
-                            String savedImageUrl = savedRecipe.getString("image");
-                            System.out.println(index + ": " + savedTitle + " - " + savedImageUrl);
-                            index ++;
-                        }
-                    } else {
-                        System.out.println("No saved recipes found.");
-                    }
-                }
+                // Refactored to use RecipeInteraction
+                recipeSaver recipeSaverInstance = new recipeSaver();
+                recipeInteraction.handleRecipeSavingAndViewing(scanner, recipes, recipeSaverInstance);
             }
         } catch (Exception e) {
-            e.printStackTrace();
-        } 
+                System.err.println("An error occurred: " + e.getMessage());
+        }
     }
 }
