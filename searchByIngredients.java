7b209bebf91e45ba7fc75f21989b6ee43fcb5df7
nickduong <160074016+nickduong@users.noreply.github.com>
2024-04-21 10:50:56 -0700

    Update searchByIngredients.java added import list

--- searchByIngredients.java
+++ searchByIngredients.java
@@ -6,6 +6,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
+import java.util.List;
 
 public class searchByIngredients {
 
